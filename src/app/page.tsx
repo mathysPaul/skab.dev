6b4d504b1ee1dc8597a94d4cd3b931824d6d7544
mathysPaul <mathys.paul@epitech.eu>
2024-11-23 17:59:10 +0100

    [FIX]: typing mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,14 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1
-        // className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
-      >
+      <h1>
         Mathys Paul
       </h1>
-      <h2
-        // className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-      >
+      <h2>
         Full Stack Developer
       </h2>
       <p>
-      In my final year of a master's degree at Epitech, I am seeking a full-time internship starting March 1st, 2024, to support the conclusion of my academic journey. Passionate about innovation, I am eager to make a meaningful contribution to your projects.
+        In my final year of a master&apos;s degree at Epitech, I am seeking a full-time internship starting March 1st, 2024, to support the conclusion of my academic journey. Passionate about innovation, I am eager to make a meaningful contribution to your projects.
       </p>
       
       <Button>Click me</Button>
